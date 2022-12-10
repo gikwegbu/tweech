af15ca3fd858e6c8bb8ea237b44d4d3a214e99cb
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2022-12-10 06:16:14 +0100

    Installed the File picker, but having some issues, trying to debug

--- lib/screens/goLive_screen.dart
+++ lib/screens/goLive_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tweech/utils/colors.dart';
+import 'package:tweech/utils/utils.dart';
 import 'package:tweech/widget/custom_button.dart';
 import 'package:tweech/widget/custom_textfield.dart';
 
@@ -14,6 +17,7 @@ class GoLiveScreen extends StatefulWidget {
 
 class _GoLiveScreenState extends State<GoLiveScreen> {
   late TextEditingController _titleController;
+  Uint8List? _thumbnail;
 
   @override
   initState() {
@@ -24,76 +28,89 @@ class _GoLiveScreenState extends State<GoLiveScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          DottedBorder(
-            borderType: BorderType.RRect,
-            radius: const Radius.circular(10),
-            dashPattern: const [10, 4],
-            strokeCap: StrokeCap.round,
-            color: btnColor,
-            child: GestureDetector(
-              onTap: () {},
-              child: Container(
-                width: double.infinity,
-                height: 150,
-                decoration: BoxDecoration(
-                  color: btnColor.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.folder_open_outlined,
-                      color: btnColor,
-                      size: 40,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "Choose video thumbnail",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 17,
+      child: GestureDetector(
+        onTap: _selectThumbnail,
+        child: _thumbnail != null
+            ? SizedBox(
+                height: 350,
+                child: Image.memory(_thumbnail!),
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    color: btnColor,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: btnColor.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.folder_open_outlined,
+                            color: btnColor,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "Choose video thumbnail",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Text(
+                    "Title",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomTextField(
+                    controller: _titleController,
+                    icon: Icons.text_fields_outlined,
+                    inputType: TextInputType.text,
+                  ),
+                  const Spacer(),
+                  CustomButton(
+                    text: 'Go Live',
+                    press: () {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            "Title",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.left,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          CustomTextField(
-            controller: _titleController,
-            icon: Icons.text_fields_outlined,
-            inputType: TextInputType.text,
-          ),
-          const Spacer(),
-          CustomButton(
-            text: 'Go Live',
-            press: () {},
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
       ),
     );
+  }
+
+  _selectThumbnail() async {
+    Uint8List? _pickedThumbnail = await imagePicker();
+    if (_pickedThumbnail != null) {
+      _thumbnail = _pickedThumbnail;
+      setState(() {});
+    }
   }
 
   @override
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 showSnackBar(BuildContext context, String content) {
@@ -8,9 +13,25 @@ showSnackBar(BuildContext context, String content) {
   );
 }
 
+// Navigation Helper Functions
 navigateTo(BuildContext context, route) {
   Navigator.pushNamed(context, route);
 }
+
 navigateAndClearPrev(BuildContext context, route) {
   Navigator.pushReplacementNamed(context, route);
+}
+
+Future<Uint8List?> imagePicker() async {
+  FilePickerResult? pickedImage = await FilePicker.platform.pickFiles(
+    type: FileType.image,
+  );
+  if (pickedImage != null) {
+    if (kIsWeb) {
+      //bytes here has value because it's on the web.
+      return pickedImage.files.single.bytes;
+    }
+    //Use path here, as it's on a mobile device.
+    return await File(pickedImage.files.single.path!).readAsBytes();
+  }
 }
